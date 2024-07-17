80cb6af6150edb006946c670b65684a0d174bf3c
Task-Over <157397575+Task-Over@users.noreply.github.com>
2024-07-17 17:06:58 +0530

    edit categories

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -11,7 +11,7 @@ const Add = ({url}) => {
         name:"",
         description:"",
         price:"",
-        category:"Salad"
+        category:"Fruits"
     })
 
     const onChangeHandler = (event) => {
