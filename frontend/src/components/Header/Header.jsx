3a44e6935242f853d8102780d9241f81683bf3f7
Task-Over <157397575+Task-Over@users.noreply.github.com>
2025-08-22 14:39:13 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your grocery here</h2>
         <p>Choose from a diverse menu featuring a number of grocery items selected and tested by the experts. Our mission is to satisfy your needs and elevate your shopping experience, one gourmet grocery at a time.</p>
-        <button>View Menu</button>
       </div>
     </div>
   )
